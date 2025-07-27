6cb985c81a14d76f9f5bded2047142c185cd1bd6
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-27 22:35:49 +0900

    HP UI, ESC 일시정지, 오류 수정, 문제풀이 중 이동 금지 구현
    -HP UI 추가(하트 이미지, LifeSystem.숫자 1,2 키로 임시 테스트 가능)
    -ESC 일시정지 기능 추가(PauseManager, PauseUI)
    -문제풀이 중 이동 금지 구현(일시정지, UI 띄우는 스크립트에 PlayerController 연결)
    -UI 계층 구조 일부 정리
    -일부 오류 수정(서랍장 이름이 스크립트와 일치하지 않아 서랍장 오브젝트 이름 변경함)
    -일부 코드 인코딩 UTF-8로 변경(체크 표시 주석), 한글 주석 깨지지 않게 함

---
+++ Assets/Script/PauseManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] PlayerController playerController;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        playerController.SetCanMove(false);  // 이동 금지
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseUI.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        playerController.SetCanMove(true);  // 이동 허용
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseUI.SetActive(false);
+    }
+
+    public void GoToHomeScene()
+    {
+        playerController.SetCanMove(true);  // 이동 허용
+        Debug.Log("홈화면으로 이동");
+
+
+        // SceneManager.LoadScene("HomeScene");
+    }
+}
