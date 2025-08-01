d3bb3aba334cb83d29d18b507b65973770bd6915
ubninin <144878603+ubninin@users.noreply.github.com>
2025-08-01 21:08:00 +0900

    함정 추가
    - 강당 맵에 구멍에 빠지는 함정 추가(LifeSystem 코드 수정)
    - 구멍에 빠질 경우 화면 페이드아웃, HP 1 감소, 강당 문앞으로 플레이어 위치 초기화, 다시 화면 페이드인

--- Assets/Script/LifeSystem.cs
+++ Assets/Script/LifeSystem.cs
@@ -1,30 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class LifeSystem : MonoBehaviour
 {
     [SerializeField] private int maxHP = 3;
     private int currentHP;
+
     [SerializeField] private Image[] heartImages;
     [SerializeField] private Sprite fullHeart;
     [SerializeField] private Sprite emptyHeart;
+
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private GameObject holeObject;
 
     private void Start()
     {
         currentHP = maxHP;
         UpdateHeartUI();
+
     }
 
-    private void Update()
-    {
-
-
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // 숫자1 누르면 hp 1 감소(임시)
-            ChangeHP(-1);
-
-        if (Input.GetKeyDown(KeyCode.Alpha2)) // 2: 증가
-            ChangeHP(1);
-    }
 
     public void ChangeHP(int amount)
     {
@@ -34,7 +31,7 @@ public class LifeSystem : MonoBehaviour
         if (currentHP == 0)
         {
             Debug.Log("Game Over");
-
+            // 게임 오버 로직 추가 가능
         }
     }
 
@@ -42,10 +39,59 @@ public class LifeSystem : MonoBehaviour
     {
         for (int i = 0; i < heartImages.Length; i++)
         {
-            if (i < currentHP)
-                heartImages[i].sprite = fullHeart;
-            else
-                heartImages[i].sprite = emptyHeart;
+            heartImages[i].sprite = i < currentHP ? fullHeart : emptyHeart;
         }
+    }
+    //구멍(함정) 추락시
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hole"))
+        {
+            Debug.Log("Hole에 떨어졌다");
+            ChangeHP(-1);
+            StartCoroutine(FadeSequence(holeObject)); 
+        }
+    }
+
+    // 화면 페이드 + 위치 초기화 + Hole 비활성화(중복처리 방지)
+    private IEnumerator FadeSequence(GameObject holeObject)
+    {
+        holeObject.SetActive(false); 
+
+        yield return StartCoroutine(FadeOut());
+        transform.position = new Vector3(22.21f, 0.188f, -15.93f);
+        yield return new WaitForSeconds(2f); 
+        yield return StartCoroutine(FadeIn());
+
+        holeObject.SetActive(true); 
+    }
+
+
+    private IEnumerator FadeOut()
+    {
+        float timer = 0f;
+        Color color = fadeImage.color;
+        while (timer < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, timer / fadeDuration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        fadeImage.color = new Color(color.r, color.g, color.b, 1f);
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float timer = 0f;
+        Color color = fadeImage.color;
+        while (timer < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, timer / fadeDuration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        fadeImage.color = new Color(color.r, color.g, color.b, 0f);
     }
 }
