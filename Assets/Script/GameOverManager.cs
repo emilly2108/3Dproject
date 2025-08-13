0fa054c870f4f56185138e695565cdc1606dea67
ubninin <144878603+ubninin@users.noreply.github.com>
2025-08-13 20:26:36 +0900

    배경음악 추가
    시작 씬, 게임오버 씬 배경음악 추가

--- Assets/Script/GameOverManager.cs
+++ Assets/Script/GameOverManager.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameOverManager : MonoBehaviour
 {
-
+    private void Start()
+    {
+        SoundManager.instance.PlayBGM("gameOverBGM");
+    }
     public void StartScene()
     {
         SceneManager.LoadScene("StartScene");
